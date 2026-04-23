3f898e4056aabed35735ee587db8771e0c5f732f
Roshan Karki <roshankarki4956@gmail.com>
2026-04-23 14:51:31 +0545

    Editing the video and hiding it

--- OpenCV/Video Functions/video_save.py
+++ OpenCV/Video Functions/video_save.py
@@ -6,7 +6,7 @@ frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 codec = cv2.VideoWriter_fourcc(*'XVID')
-recorder = cv2.VideoWriter("Videos/My_Video", codec, 20, (frame_width, frame_height))
+recorder = cv2.VideoWriter("Videos/My_Video.avi", codec, 20, (frame_width, frame_height))
 
 while True:
     success, image = camera.read()
