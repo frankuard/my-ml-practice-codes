ac3dae547de15a2dfea1724e6de644663e838b3a
Roshan Karki <roshankarki4956@gmail.com>
2026-04-25 14:25:32 +0545

    intializing mediapipe

---
+++ MediaPipe/Hand Tracking/handTrackingMin.py
@@ -0,0 +1,19 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    
+    ret, frame = cap.read()
+    
+    frame = cv2.flip(frame,1)
+    
+    cv2.imshow("Video Movement", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
