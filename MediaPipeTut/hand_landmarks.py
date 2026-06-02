fd1ecf2535a8561736cfbbf158f4695d32fd5339
Roshan Karki <roshankarki4956@gmail.com>
2026-06-02 22:31:37 +0545

    fINALIZING THE LANDMAKR SYSTEM FOR THE LAND

--- MediaPipeTut/hand_landmarks.py
+++ MediaPipeTut/hand_landmarks.py
@@ -38,6 +38,7 @@ while True:
     if not ret:
         break
     
+    frame = cv2.flip(frame,1)
     ## CONVERTING BGR TO RGB
     
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -45,6 +46,7 @@ while True:
     mp_image = mp.Image(image_format = mp.ImageFormat.SRGB,data= frame_rgb)
     
     timestamp_ms = int(time.time()* 1000)
+    
     landmarker.detect_async(mp_image,timestamp_ms)
     
     
@@ -54,9 +56,40 @@ while True:
     ## PRINTING FOUND LANDMARK IF IT FINDS IT
     
     if result and result.hand_landmarks:
-        print("Landmarks Found")
         
-    
+        
+        hand_lms = result.hand_landmarks[0]
+        
+        h,w = frame.shape[:2]
+        
+        
+        ## FOR CONNECTING LINES
+        
+        Connections = [(0,1),(1,2),(2,3),(3,4),(0,5),(5,6),(6,7),(7,8),(0,9),(9,10),(10,11),(11,12),(0,13),(13,14),(14,15),(15,16),(0,17),(17,18),(18,19),(19,20)]
+        
+        
+        for a,b in Connections:
+            x1 = int(hand_lms[a].x*w)
+            y1 = int(hand_lms[a].y*h)
+            
+            x2 = int(hand_lms[b].x*w)
+            y2 = int(hand_lms[b].y*h)
+
+            cv2.line(frame,(x1,y1),(x2,y2),(255,255,255),2)
+
+        
+        landmarkers = list(range(21))
+        
+        for i in landmarkers:
+            lm= hand_lms[i]
+            x = int(lm.x*w)
+            y = int(lm.y*h)
+            
+            cv2.circle(frame,(x,y),5,(0,200,0),-1)
+
+            cv2.putText(frame, str(i),(x+5,y-5),cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255),1)
+            
+            
     cv2.imshow("Hand Tracking", frame)
     
     if cv2.waitKey(1) & 0xFF== ord('q'):
@@ -66,4 +99,5 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 landmarker.close()
+
 
